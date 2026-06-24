260fae47ffbc356e4b35736f35d417b8458d770c
Marcelo Pereira <marcelopereira4vsc@gmail.com>
2026-06-24 13:49:11 +0100

    updates

---
+++ trustpilot_sentiment_dashboard/app.py
@@ -0,0 +1,9 @@
+# dashboard that helps to analyze the sentiment of reviews from Trustpilot using the TextBlob library
+
+## Importing necessary libraries
+import streamlit as st 
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+
